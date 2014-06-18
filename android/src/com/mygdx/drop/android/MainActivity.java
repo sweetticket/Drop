faf577e81078fbe625940440b6dbd420a82de73b
jenny <yk494@cornell.edu>
2014-06-18 11:27:30 +0900

    drop game done

--- android/src/com/mygdx/drop/android/MainActivity.java
+++ android/src/com/mygdx/drop/android/MainActivity.java
@@ -6,11 +6,15 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.mygdx.drop.Drop;
 
-public class AndroidLauncher extends AndroidApplication {
+public class MainActivity extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		
 		initialize(new Drop(), config);
 	}
 }
--- desktop/src/com/mygdx/drop/desktop/Main.java
+++ desktop/src/com/mygdx/drop/desktop/Main.java
@@ -4,9 +4,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.drop.Drop;
 
-public class DesktopLauncher {
+public class Main {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Drop(), config);
+		config.title = "Drop";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new Drop(), config);		
 	}
 }
